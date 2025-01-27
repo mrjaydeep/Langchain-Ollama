49681e87d8ea1eab13f3f7d52f7d2b048be63537
Jaydeep1310 <101039415+mrjaydeep@users.noreply.github.com>
2025-01-27 15:35:05 +0530

    Update locallama.py

--- Chatbot/locallama.py
+++ Chatbot/locallama.py
@@ -19,7 +19,7 @@ prompt=ChatPromptTemplate.from_messages(
 )
 ## streamlit framework
 
-st.title('Langchain Demo With LLAMA2 API')
+st.title('Langchain Demo With LLAMA3.2 API')
 input_text=st.text_input("Search the topic u want")
 
 # ollama LLAma2 LLm 
